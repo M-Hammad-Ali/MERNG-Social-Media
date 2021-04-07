1ee158092b84716c6ea4dc50770e7ab1a0dce948
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-04-07 23:32:15 +0500

    delete comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -30,6 +30,28 @@ module.exports = {
                     body:"Invalid Post ID"
                 }
             })
+        },
+
+        deleteComment: async (_,{postId,commentId},context) =>{
+            const user = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post){
+                const commentIndex = post.comments.findIndex(c => c.id === commentId)
+
+                if(post.comments[commentIndex].username === user.username){
+                    post.comments.splice(commentIndex,1);
+                    await post.save();
+                    return post;
+                }
+                else {
+                    throw new UserInputError("User is not allowed to deleted this comment");
+                }
+            }
+            else {
+                throw new UserInputError("Invalid Post Id");
+            }
+            post.comments
         }
     }
 }
