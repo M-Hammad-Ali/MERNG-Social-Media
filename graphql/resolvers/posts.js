b5c55dbbd25a9af325f2ff7c17d8f6013106793e
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-04-07 10:16:31 +0500

    create post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check-auth');
 
 module.exports = {
     Query : {
@@ -23,6 +24,21 @@ module.exports = {
             }catch(err){
                 throw new Error(err)
             }
+        }
+    },
+    Mutation: {
+        async createPost(_,{body},context){
+            const user = checkAuth(context);
+            
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString(),
+            })
+
+            const post = await newPost.save();
+            return post;
         }
     }
 }
