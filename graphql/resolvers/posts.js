62eb0197d71713b338466bc96ae5348799ec5f30
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-04-07 10:44:21 +0500

    delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,4 @@
+const {AuthenticationError} = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
 
@@ -5,7 +6,7 @@ module.exports = {
     Query : {
         async getPosts(){
             try {
-                const posts = await Post.find();
+                const posts = await Post.find().sort({createdAt:-1});
                 return posts;
             }
             catch(err) {
@@ -39,6 +40,20 @@ module.exports = {
 
             const post = await newPost.save();
             return post;
+        },
+        async deletePost(_,{postId},context){
+            const user = checkAuth(context);
+            try {
+                const post =await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return "Post Deleted Successfully";
+                }else {
+                    throw new AuthenticationError("Action Not Allowed!")
+                }
+            }catch(err){
+                throw new Error(err);
+            }
         }
     }
 }
