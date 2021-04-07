f89ba261fc84a16ecaee3e05c53a3b1ba3f89692
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-04-08 00:26:06 +0500

    like and unlike post

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -51,7 +51,26 @@ module.exports = {
             else {
                 throw new UserInputError("Invalid Post Id");
             }
-            post.comments
+        },
+
+        likePost : async(_,{postId},context) => {
+            const user = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post){
+                if(post.likes.find(like => like.username === user.username)) {
+                    post.likes = post.likes.filter(like=> like.username !== user.username);
+                }else {
+                    post.likes.push({
+                        username:user.username,
+                        createdAt:new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            }else {
+                throw new UserInputError("Invalid Post");
+            }
         }
     }
 }
