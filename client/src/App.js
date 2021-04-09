548d58b74c681a8770978002d56785c71fe2163c
m-hammad-ali <mohammadhammad0358@gmail.com>
2021-04-09 22:16:02 +0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <h1>YO!</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+    </Router>
   );
 }
 
